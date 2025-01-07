d2d71c9bf4f1edbc1bdc488422c27a115097f5c6
Matt Stirling <stirlingmattmarkus@gmail.com>
2025-01-08 00:12:17 +0200

    Added 2 libraries from MyUtilities

---
+++ util/__init__.py
@@ -0,0 +1,4 @@
+from .json_handler import JsonHandler
+from .bookmarks_getter import BookmarksGetter
+
+__all__ = ["JsonHandler", "BookmarksGetter"] # What gets imported with 'from my_utilities import *'
